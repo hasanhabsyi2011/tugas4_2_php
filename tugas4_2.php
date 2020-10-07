bb5625cbd83ce4ca3e0b921941ef6ab4f8b518f0
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-07 16:39:56 +0700

    Add files via upload

---
+++ tugas4_2.php
@@ -0,0 +1,13 @@
+<?php 
+
+$saluran = 523;
+$terpakai = 8891;
+$rerata = $terpakai/$saluran;
+
+echo "Jumlah Saluran = ", $saluran, "<br>" ; 
+echo "Jumlah terpakai = ", $terpakai, "<br>" ;
+echo "Rata-rata Pemakaian = ....? <br>";
+echo "Jawaban = ", $rerata, " liter/keluarga"
+
+
+ ?>
